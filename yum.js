63af41b69de099a24ee6c477fb8470bb69c8b1ab
terb <bretgeek@gmail.com>
2021-02-03 22:20:43 -0700

    added props object to render, now components can take props

--- yum.js
+++ yum.js
@@ -2016,11 +2016,11 @@ function yum(itr, ...Arr) {// itr = strings of things to iterate over
   }
 
   // App, to=element {options [pos=position, reactor = create a reactor, set reactor element to atom  }
-  function _render(fn, to='body', { pos='append', reactor=false, state='state', initState=false,  } ) {
+  function _render(fn, to='body', { pos='append', reactor=false, state='state', initState=false, props=false } ) {
     // check if function
     let h;
     if (isFunction(fn)) {
-      h = fn();// returns the html
+      h = fn(props);// returns the html
      // console.log('app name is '+fn.name);
     if(reactor){
     yum(h).Reactor();
