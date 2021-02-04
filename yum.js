830afbac82873f04ce68764d88add1e484bafca3
terb <bretgeek@gmail.com>
2021-02-04 05:42:06 -0700

    set reactor to default as true for component syntax

--- yum.js
+++ yum.js
@@ -2016,7 +2016,7 @@ function yum(itr, ...Arr) {// itr = strings of things to iterate over
   }
 
   // App, to=element {options [pos=position, reactor = create a reactor, set reactor element to atom  }
-  function _render(fn, to='body', { pos='append', reactor=false, state='state', initState=false, props=false } ) {
+  function _render(fn, to='body', { pos='append', reactor=true, state='state', initState=false, props=false } ) {
     // check if function
     let h;
     if (isFunction(fn)) {
