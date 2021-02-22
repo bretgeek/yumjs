c53d45bc3b704fd4ba8500b817670cbe95d6b298
terb <bretgeek@gmail.com>
2021-02-22 12:29:28 -0700

    aliased fq to plug

--- yum.js
+++ yum.js
@@ -2160,6 +2160,7 @@ function yum(itr, ...Arr) {// itr = strings of things to iterate over
     ready: ready,
     delay: delay,
     plug: plug,
+    fq: plug,
     spy: spy,
     unspy: unspy,
     trigger: trigger,
