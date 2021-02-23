84f4c1c5a0d6b65eb007991074dc9efc20c5dd3a
terb <bretgeek@gmail.com>
2021-02-22 21:45:10 -0700

    refactor fx/plug

--- yum.js
+++ yum.js
@@ -1858,42 +1858,40 @@ function yum(itr, ...Arr) {// itr = strings of things to iterate over
 
   // PLUGIN
   // function plug(fn, time=10, step=1, iterate=1) {
-  function plug(fn, { delay = 10, step = 1, iterate = 1, done = false} = 1) {
+  function plug(fn, {delay = 10, step = 1, iterate = 1} = 1) {
     for ( const e of _stk) {
       if (iterate <= 1) {
         setTimeout((t)=>{
           // console.log(`running ${ delay} with ${step}`)
           fn(e, step); // the plugged in function gets passed these params but you don't have to use them
-        },  delay);
+        }, delay);
       }
-      
-    e.farr = e.farr || [];
+
+      e.farr = e.farr || [];
       if (iterate > 1) {
         for (let i = 0; i < iterate; i++) {
           e.farr.push(fn);
         }
       }
-      let pinc = 0;
+
       if (e.farr.length) {
-        for (f of e.farr) {
-          if (pinc > 0 ) {
-             delay =  delay * 2; // delay must double for subsequent calls
+        let intv;
+        intv = setInterval((t)=>{
+          // console.log('iterating');
+          e.step = e.step || step;
+          if (!e.farr.length) {
+            clearInterval(intv);
+            e.step = 0;
           }
-          pinc++;
-
-       let intv;
-       intv = setInterval((t)=>{
-            // console.log('iterating');
-            f(e, step);
-              if(pinc >= e.farr.length){
-                clearInterval(intv)
-              }
-          },  delay);
-    window.onblur = function() {
-      clearInterval(intv)
-    };
-
-        }
+          if (e.farr.length) {
+            e.farr[0](e, step);
+            e.farr.shift();
+            // console.log('len is '+e.farr.length);
+          }
+        }, delay);
+        window.onblur = function() {
+          clearInterval(intv);
+        };
       }
     }
     return this;
